d4e4e70177c083a4a7d9ab79cae5333240770ea6
Atilim Gunes Baydin <atilimgunes.baydin@gmail.com>
2024-07-11 23:33:52 +0100

    aia degradation correction

--- process_sdo.py
+++ process_sdo.py
@@ -11,7 +11,7 @@ import numpy as np
 from glob import glob
 
 def process(args):
-    source_file, target_file, resolution = args
+    source_file, target_file, resolution, degradations = args
 
     try:
         Xd = Map(source_file)
@@ -26,8 +26,14 @@ def process(args):
     validMask = 1.0 * (X > 0) 
     X[np.where(X<=0.0)] = 0.0
 
+    fn = os.path.basename(source_file)
+    fn2 = fn.split("_")[0].replace("AIA","")
+    datestring = "%s-%s-%s" % (fn2[:4],fn2[4:6],fn2[6:8])
+    wavelength = int(fn.split("_")[-1].replace(".fits",""))
+
     expTime = max(Xd.meta['EXPTIME'],1e-2)
-    quality = Xd.meta['QUALITY']    
+    quality = Xd.meta['QUALITY']
+    correction = degradations[wavelength][datestring]
 
     if quality != 0:
         print('Quality flag is not zero: {}'.format(quality))
@@ -50,9 +56,7 @@ def process(args):
     #correct for interpolating over valid pixels
     Xr = np.divide(Xr,(Xd+1e-8))
 
-    # SKIPPING DEGRADATION CORRECTION, MIGHT REVISIT
     #correct for exposure time and AIA degradation correction
-    correction = 1.
     Xr = Xr / (expTime*correction)
 
     #figure out the integer factor to downsample by mean
@@ -71,6 +75,21 @@ def process(args):
     print('Target: {}'.format(target_file))
     return True
 
+def load_degradations(degradation_dir, wavelengths):
+    def getDegrad(fn):
+        #map YYYY-MM-DD -> degradation parameter
+        lines = open(fn).read().strip().split("\n")
+        degrad = {}
+        for l in lines:
+            d, f = l.split(",")
+            degrad[d[1:11]] = float(f)
+        return degrad     
+    #return wavelength -> (date -> degradation dictionary)
+    degrads = {} 
+    for wl in wavelengths:
+        degrads[wl] = getDegrad(os.path.join(degradation_dir, 'degrad_{}.csv'.format(wl)))
+    return degrads 
+
 
 def main():
     description = 'FDL-X 2024, Radiation team, SDO AIA data process script'
@@ -80,6 +99,8 @@ def main():
     parser.add_argument('--max_workers', type=int, default=4, help='Max workers')
     parser.add_argument('--worker_chunk_size', type=int, default=1, help='Chunk size per worker')
     parser.add_argument('--resolution', type=int, default=512, help='Pixel resolution of processed images. Should be a divisor of 1024.')
+    parser.add_argument('--wavelengths', nargs='+', default=[94,131,171,193,211,304,335,1600,1700], help='Wavelengths')
+    parser.add_argument('--degradation_dir', type=str, default='./degradation', help='Directory with degradation correction files')
 
     args = parser.parse_args()
 
@@ -90,6 +111,9 @@ def main():
     print('Arguments:\n{}'.format(' '.join(sys.argv[1:])))
     print('Config:')
     pprint.pprint(vars(args), depth=2, width=50)
+
+    print('Loading degradations')
+    degradations = load_degradations(args.degradation_dir, args.wavelengths)
 
     # walk through the source directory with glob, find all .fits files, and create a corresponding file name ending in .npz in the target dir, keeping the directory structure
 
@@ -105,7 +129,7 @@ def main():
     file_names = []
     for source_file in fits_files:
         target_file = source_file.replace(source_dir, target_dir).replace('.fits', '.npz')
-        file_names.append((source_file, target_file, args.resolution))
+        file_names.append((source_file, target_file, args.resolution, degradations))
 
     # process the files
     results = process_map(process, file_names, max_workers=args.max_workers, chunksize=args.worker_chunk_size)
