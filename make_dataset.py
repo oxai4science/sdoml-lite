03e3fe95c25ae9a424e8cf9ca41ba9553f7b2ab8
Atilim Gunes Baydin <atilimgunes.baydin@gmail.com>
2024-07-17 15:38:21 +0100

    webdataset in units of days

--- make_dataset.py
+++ make_dataset.py
@@ -12,7 +12,7 @@ def main():
     parser = argparse.ArgumentParser(description=description)
     parser.add_argument('--source_dir', type=str, help='Source directory', required=True)
     parser.add_argument('--target_dir', type=str, help='Destination directory', required=True)
-    parser.add_argument('--files_per_archive', type=int, default=None, help='Number of files per archive')
+    parser.add_argument('--days_per_archive', type=int, default=1, help='Number of days per archive')
     parser.add_argument('--prefix', type=str, default=None, help='Prefix for tar files')
 
     args = parser.parse_args()
@@ -44,76 +44,87 @@ def main():
     # Find all files in the directory
     files = sorted([file for file in glob(os.path.join(source_dir, '**', '*'), recursive=True) if os.path.isfile(file)])
 
-    files_per_archive = args.files_per_archive
-    if files_per_archive is None:
-        files_per_archive = 1 + len(files) // 2
-        print(f"Number of files per archive not specified, defaulting to {files_per_archive}")
-    if files_per_archive < 1:
-        print("Number of files per archive must be greater than 0.")
-        sys.exit(1)
-    elif files_per_archive > len(files):
-        print("Number of files per archive must be less than the total number of files.")
-        sys.exit(1)
-
-    # Calculate how many digits needed to zero-pad file names
-    total_files = len(files)
-    num_archives = (total_files + files_per_archive - 1) // files_per_archive
-    padding_length = len(str(num_archives))
-
+    print()
     # Size of all data under source directory
     total_size = sum(os.path.getsize(file) for file in files)
-    archive_size = total_size // num_archives
+    print(f"Total number of files found: {len(files):,}")
+    print(f"Total size of data         : {total_size:,} bytes")
 
-    print(f"Total number of files found       : {total_files:,}")
-    print(f"Number of files per archive (max) : {files_per_archive:,}")
+    file_earliest = os.path.relpath(files[0], source_dir)
+    file_latest = os.path.relpath(files[-1], source_dir)
+    print(f"Earliest file: {file_earliest}")
+    print(f"Latest file  : {file_latest}")
+
+    date_earliest = datetime.datetime.strptime(file_earliest[:10], '%Y/%m/%d')
+    date_latest = datetime.datetime.strptime(file_latest[:10], '%Y/%m/%d')
+
+    #print earliest date without time information
+    print(f"Earliest date: {date_earliest.strftime('%Y-%m-%d')}")
+    print(f"Latest date  : {date_latest.strftime('%Y-%m-%d')}")
+    print(f'Total days   : {(date_latest - date_earliest).days + 1:,}')
+
+    # iterate over days
+
+    num_archives = ((date_latest - date_earliest).days + args.days_per_archive)// args.days_per_archive
     print(f"Number of archives to be generated: {num_archives:,}")
-    print(f"Total size of data                : {total_size:,} bytes")
-    print(f"Approx. size of data per archive  : {archive_size:,} bytes")
 
-    # Loop to create tar files
-    for i in range(0, total_files, files_per_archive):
-        # Calculate the index for naming the tar file
-        index = (i // files_per_archive) + 1
+    padding_length = len(str(num_archives))
 
-        # Format the index with zero padding
+    current = date_earliest
+    while current <= date_latest:
+        current_end = current + datetime.timedelta(days=args.days_per_archive - 1)
+        if current_end > date_latest:
+            current_end = date_latest
+
+        # calculate the index for naming the tar file
+        index = (current - date_earliest).days // args.days_per_archive + 1
+        # format the index with zero padding
         formatted_index = str(index).zfill(padding_length)
-
         tar_filename = f"{prefix}-{formatted_index}.tar"
         tar_filepath = os.path.join(target_dir, tar_filename)
 
-        print(f"Generating file         : {tar_filename}")
+        print()
+        print(f"Archive               : {tar_filename} ({index}/{num_archives})")
+        print(f"Date range (inclusive): {current.strftime('%Y-%m-%d')} - {current_end.strftime('%Y-%m-%d')}")
 
-        # Create a tar file for this chunk
         with tarfile.open(tar_filepath, "w") as tar:
-            for file in files[i:i+files_per_archive]:
-                arcname = os.path.relpath(file, source_dir)
-                # print('Source: {}'.format(arcname))
-                arcname_dir = os.path.dirname(arcname)
-                arcname_base = os.path.basename(arcname)
+            while current <= current_end:
+                print(f"Adding date: {current.strftime('%Y-%m-%d')}")
 
-                if arcname_base.startswith('AIA'):
-                    _, time, wavelength = arcname_base.split('_')
-
-                    if time.endswith('14'):
-                        time = time[:2] + '15'
-                    elif time.endswith('44'):
-                        time = time[:2] + '45'
-
-                    wavelength = wavelength.split('.')[0]
-                    arcname_base = f"{time}.AIA_{wavelength}.npy"
-                elif arcname_base.startswith('HMI'):
-                    _, time, _ = arcname_base.split('_')
-                    arcname_base = f"{time}.HMI_M.npy"
+                files_to_add = [file for file in files if file.startswith(os.path.join(source_dir, current.strftime('%Y/%m/%d')))]
+                if len(files_to_add) == 0:
+                    print(f"No files found for {current.strftime('%Y-%m-%d')}")
                 else:
-                    print(f"Unknown file format: {arcname_base}")
-                    continue
+                    for file in files_to_add:
+                        arcname = os.path.relpath(file, source_dir)
+                        # print('Source: {}'.format(arcname))
+                        arcname_dir = os.path.dirname(arcname)
+                        arcname_base = os.path.basename(arcname)
 
-                arcname = os.path.join(arcname_dir, arcname_base)
-                # print('Target: {}'.format(arcname))
+                        if arcname_base.startswith('AIA'):
+                            _, time, wavelength = arcname_base.split('_')
 
-                tar.add(file, arcname=arcname)
+                            if time.endswith('14'):
+                                time = time[:2] + '15'
+                            elif time.endswith('44'):
+                                time = time[:2] + '45'
 
-        print(f"Finished generating file: {tar_filename} ({index}/{num_archives}) ({os.path.getsize(tar_filepath):,} bytes)")
+                            wavelength = wavelength.split('.')[0]
+                            arcname_base = f"{time}.AIA_{wavelength}.npy"
+                        elif arcname_base.startswith('HMI'):
+                            _, time, _ = arcname_base.split('_')
+                            arcname_base = f"{time}.HMI_M.npy"
+                        else:
+                            print(f"Unknown file format: {arcname_base}")
+                            continue
+
+                        arcname = os.path.join(arcname_dir, arcname_base)
+                        # print('Target: {}'.format(arcname))
+
+                        tar.add(file, arcname=arcname)
+                current += datetime.timedelta(days=1)
+
+            print(f"Archive complete      : {tar_filename} ({os.path.getsize(tar_filepath):,} bytes)")
 
     print("Archives created successfully.")
 
