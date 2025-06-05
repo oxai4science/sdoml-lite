43b2edc45ac4489ba988fb7d48a24d141d548e78
Atilim Gunes Baydin <atilimgunes.baydin@gmail.com>
2025-06-05 20:36:01 +0100

    increase timeouts

--- get_aia.py
+++ get_aia.py
@@ -32,7 +32,7 @@ def process(args):
 
     print('Remote: {}'.format(remote_file_name), flush=True)
     os.makedirs(os.path.dirname(local_file_name), exist_ok=True)
-    timeout = 5 # seconds
+    timeout = 10 # seconds
     retries = 5
     for i in range(retries):
         if i > 0:
